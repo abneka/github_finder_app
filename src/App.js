dc3546aa299790d7a384d6d7a5f67e2a2901c105
ABESOLOM NEKAHIWOT <88771382+abneka@users.noreply.github.com>
2023-07-20 14:43:27 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -9,9 +9,6 @@ import NotFound from './pages/NotFound'
 import { GitProvider } from './context/github/GitContext'
 import { AlertProvider } from './context/alert/AlertContext'
 
-// NOTE: Alert is only used on the '/' route moving to that route we can prevent
-// content shift when alert shows by hiding and unhiding the Alert rather than
-// conditionally rendering
 
 const App = () =>  {
   return (
